2d4777087032108732fe03ba36615053ca9aa52b
AurelioB <aurelio@bernalr.com>
2013-02-27 23:12:32 -0600

    Add options menu

--- src/BMPBorder.c
+++ src/BMPBorder.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 #include <sched.h>
-#include <assert.h>
+#include <unistd.h>
+#include <ctype.h>
 #include <sys/sysinfo.h>
 
 #define DIF 16
@@ -17,8 +18,7 @@
 #define CLONE_FILES     0x00000400      /* set if open files shared between processes */
 #define CLONE_SIGHAND   0x00000800      /* set if signal handlers shared */
 
-// NOMBRE DEL ARCHIVO A PROCESAR
-char filename[]="test.bmp";
+
 
 #pragma pack(2)  // Empaquetado de 2 bytes
 typedef struct {
@@ -54,6 +54,7 @@ typedef struct {
 } IMAGE;
 
 IMAGE imagenfte, imagendst;
+int imageRows, imageCols;
 
 int loadBMP(char *filename, IMAGE *image) {
   	FILE *fin;
@@ -75,7 +76,7 @@ int loadBMP(char *filename, IMAGE *image) {
 	fread(&image->infoheader, sizeof(INFOHEADER), 1, fin);
    	// Probar si es un archivo BMP 24 bits no compactado
 	if (!((image->infoheader.bitsPerPixel == 24) && !image->infoheader.compression)) {
-		printf("	ERROR: BMP file is in not in 24 bits, uncompressed format (%d bits)\n", image->infoheader.bitsPerPixel);
+		printf("	WARNING: BMP file is not 24 bits, uncompressed (%d bits)\n", image->infoheader.bitsPerPixel);
 		//return(-1);
 	}
 	printf("-- Width: %d\n-- Height: %d\n", image->infoheader.cols, image->infoheader.rows);
@@ -84,6 +85,10 @@ int loadBMP(char *filename, IMAGE *image) {
    	while(i < totpixs) {
 		fread(image->pixel + i, sizeof(PIXEL), 512, fin); i+=512;
    	}
+
+   	imageRows = image->infoheader.rows;
+	imageCols = image->infoheader.cols;
+
    	fclose(fin);
    	return 0;
 }
@@ -118,13 +123,12 @@ void processBMP(IMAGE *imagefte, IMAGE *imagedst, int y0, int y1) {
 	int count = 0;
 	PIXEL *pfte, *pdst;
 	PIXEL *v0, *v1, *v2, *v3, *v4, *v5, *v6, *v7;
-	int imageRows, imageCols;
+	
 	//memcpy(imagedst,imagefte,sizeof(IMAGE)-sizeof(PIXEL *));
-	imageRows = imagefte->infoheader.rows;
-	imageCols = imagefte->infoheader.cols;
+	
 	//imagedst->pixel=(PIXEL *)malloc(sizeof(PIXEL)*imageRows*imageCols);
     //for( i = 1; i < imageRows - 1; i++ )
-    printf("Y0: %d, Y1: %d\n", y0, y1);
+    //printf("Y0: %d, Y1: %d\n", y0, y1);
     for( i = y0; i < (y1 - 1); i++ )
 		for( j = 1; j < (imageCols - 1); j++ ){
 			pfte = imagefte->pixel + imageCols * i + j;
@@ -150,7 +154,7 @@ void processBMP(IMAGE *imagefte, IMAGE *imagedst, int y0, int y1) {
 				pdst->red = 0;
 				pdst->green = 0;
 				pdst->blue = 0;
-			} else  /*if(i > y0 && i < y1)*/ {
+			} else  {
 				pdst->red = 255;
 				pdst->green = 255;
 				pdst->blue = 255;
@@ -159,77 +163,100 @@ void processBMP(IMAGE *imagefte, IMAGE *imagedst, int y0, int y1) {
 }
 
 int run(int *argv) {
-	printf("\n---\nThread from %d to %d\n---\n", argv[0], argv[1]);
+	//printf("\n---\nThread from %d to %d\n---\n", argv[0], argv[1]);
 	processBMP(&imagenfte, &imagendst, argv[0], argv[1]);
 	free(argv);
 	return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
 	void *child_stack;
 	child_stack = (void *) malloc(SIZE);
-  	assert(child_stack != NULL);
   	int pid, status, rc;
-  	printf("%d cores available\n", get_nprocs());
-	int nThreads = get_nprocs();
+  	int opt;
+	char *filename = "input.bmp";
+	char *destination = "output.bmp";
 
+	int nThreads = get_nprocs();	// Number of threads. By default equal to the number of available cores
+
+  	while ((opt = getopt (argc, argv, "n:i:o:")) != -1) {
+    	switch (opt) {
+           	case 'n':
+            	nThreads = strtol(optarg, (char **)NULL, 10);
+             	break;
+           	case 'i':
+             	filename = optarg;
+             	break;
+           	case 'o':
+             	destination = optarg;
+             	break;
+           	case '?':
+             	if (optopt == 'n' || optopt == 'i' || optopt == 'o')
+               		fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+             	else if (isprint (optopt))
+               		fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+             	else
+               		fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+             	return 1;
+           	default:
+            	abort ();
+		}
+	}
+
+	if(argc == 1) {
+		printf("\nBMP border is a multithreaded tool to detect edges in BMP images.\n\n");
+		printf("The following options are available:\n\n");
+		printf("	-n	<number>	Number of threads to create\n");
+		printf("				DEFAULT: Number of available cores\n\n");
+		printf("	-i	<file>		Input file to read\n");
+		printf("				DEFAULT: input.bmp\n\n");
+		printf("	-o	<file>		Output file to write\n");
+		printf("				DEFAULT: output.bmp\n\n");
+	}
+  	
 	int res;
 	clock_t t_inicial,t_final;
-	char namedest[80];
+	
+
 	t_inicial=clock();
-	strcpy(namedest,strtok(filename,"."));
-	strcat(filename,".bmp");
-	strcat(namedest,"_P.bmp");
-	printf("-- Source: %s\n",filename);
-	printf("-- Output: %s\n",namedest);
+
+
+
+
+	printf("-- Input File: %s\n",filename);
+	
 	res = loadBMP(filename, &imagenfte);
 	if(res == -1) {
 		fprintf(stderr,"	ERROR: Can't open source file\n");
 		exit(1);
 	}
 	initBMP(&imagenfte, &imagendst);
-	//printf("Procesando imagen de: Renglones = %d, Columnas = %d\n", imagenfte.infoheader.rows, imagenfte.infoheader.cols);
-	/*initBMP(&imagenfte, &imagendst);
-	int args[2];
-	args[0] = 1;
-	args[1] = 101;
-	pid = clone(run, child_stack + SIZE/sizeof(void *), 0x00000100, args);
-	int args2[2];
-	args2[0] = 100;
-	args2[1] = 201;
 
-	pid = clone(run, child_stack + (SIZE * 2) /sizeof(void *), 0x00000100, args2);*/
 	int n;
 	int space = imagenfte.infoheader.rows / nThreads;
+	printf("-- Cores available: %d\n", get_nprocs());
+	printf("-- Thread execution (%d Total):\n", nThreads);
 	for(n = 1; n <= nThreads; n++) {
-		//int args[2];
-		int *args = (int*) malloc(2 * sizeof(int));
+		int *args = (int*) malloc(2 * sizeof(int));	// malloc space for clone arguments
 		args[0] = space * (n - 1);
 		args[1] = args[0] + space + 1;
-		printf("Thread %d: %d - %d\n", n, args[0], args[1]);
-		pid = clone(run, child_stack + (SIZE/sizeof(void *)) * n, 0x00000100, args);
+		pid = clone(run, child_stack + (SIZE/sizeof(void *)) * n, CLONE_VM, args);
 	}
 
-	//assert(pid != -1);
   	status = 0;
-  	while(rc != -1) {
+
+  	do {
 		rc = waitpid(-1, &status, __WCLONE);
-		printf("THREAD RC: %d\n", rc);
-	}
-
-	//assert(rc != -1);
-  	//assert(WEXITSTATUS(status) == 0);
-
-	//processBMP(&imagenfte, &imagendst, 1, 50);
-	//processBMP(&imagenfte, &imagendst, 100, 150);
-	//processBMP(&imagenfte, &imagendst, 300, 400);
-	//processBMP(&imagenfte, &imagendst, 200, 300);
-	//processBMP(imagenfte, &imagendst, 1, 200);
-	res = saveBMP(namedest, &imagendst);
+		if(rc == -1)
+			break;
+		printf("	Thread with PID %d finished execution\n", rc);
+	} while(1);
+	printf("-- Output File: %s\n",destination);
+	res = saveBMP(destination, &imagendst);
 	if(res == -1) {
 		fprintf(stderr,"	ERROR: Can't write output file\n");
 		exit(1);
 	}
 	t_final = clock();
-	printf("Tiempo %3.6f segundos\n",((float) t_final- (float)t_inicial)/ CLOCKS_PER_SEC);
+	printf("-- Time %3.6f seconds\n",((float) t_final- (float)t_inicial)/ CLOCKS_PER_SEC);
 }
